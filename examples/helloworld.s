00b8f85cc46b293273a6a37f6d9e9c6ef3d24d8c
Scott LaVigne <pyrated@gmail.com>
2013-10-11 05:39:10 -0700

    Add syntax highlighter for sublime in extras

--- examples/helloworld.s
+++ examples/helloworld.s
@@ -1,10 +1,9 @@
-# hello world in ATOMS
-# PUS
+# Hello world in ATOMS
 .entry 0 0 1
 
-  .const 'Hello World!'  # K[0] = 'Hello World!'
-  loadk 0 0              # R[0] = K[0]
-  puts 0                 # print R[0]
-  ret 0 1                # return nil
-
+  .const  'Hello buuudy!'
+  loadk   0 0
+  puts    0
+  ret     0 1
+  
 .end
