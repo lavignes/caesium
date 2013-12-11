7aafd05c4f64ce204b8a12f593c69204d832ed42
Scott LaVigne <pyrated@gmail.com>
2013-12-11 07:04:59 -0800

    examples

--- examples/42.s
+++
@@ -1,9 +0,0 @@
-# Set global 'QΔЙקم๗あ叶葉말 :]' to 42
-# PUS: params upvals stacks 
-.entry 0 0 1
-  .const 'QΔЙקم๗あ叶葉말 :]'
-  .const 42
-  loadk 0 1
-  storg 1 0
-  ret 0 1
-.end
--- examples/helloworld.s
+++ examples/helloworld.s
@@ -1,31 +1,8 @@
-.entry 0 0 3
-
-  .func 2 0 1
-    add 0 0 1
-    ret 0 2
-    .resq
-      .const 'From add function: '
-      .const 'what'
-      catch 0
-      loadi 1 0 257
-      add 1 256 1
-      stori 1 0 257
-      raise 0
-    .end
-  .end
-
-  .const 2
-  .const 'test'
-
-  clos 0 0
-  loadk 1 0
-  loadk 2 1
-
-  call 0 2 2
+#
+# print 'Hey Buddy!'
+#
+.entry 0 0 1
+  .const 'Hey Buddy!'
+  loadk 0 0
   puts 0
-
-  .resq
-    catch 0
-    puts 0
-  .end
 .end
